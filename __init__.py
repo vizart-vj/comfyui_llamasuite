84cdcc5d4c3487905a269ca9289cb54598528fcc
vizart-vj <62356719+vizart-vj@users.noreply.github.com>
2026-06-15 15:35:19 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
 from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 from .server_routes import setup_routes
 
+# Инициализация маршрутов сервера
 setup_routes()
 
+# Указание папки с фронтендом
 WEB_DIRECTORY = "./js"
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
